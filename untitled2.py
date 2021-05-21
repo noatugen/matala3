48df3adda7393f2ccae2cda97c6ba462ba1e3984
noatugen <noaz99@walla.com>
2021-05-21 14:31:36 +0300

    new

--- untitled2.py
+++ untitled2.py
@@ -1,5 +1,10 @@
 import json 
  
+def read_file():
+    file = open("WhatsApp2.txt" ,encoding='utf8')
+    return file
+           
+        
 def what_App(): 
     file = read_file()  
     number_id=1
@@ -20,12 +25,14 @@ def what_App():
         first=line.find('-')+1
         stop=line.find(':',first)        
         if stop>0:    
-            name=line[first:stop]        
+            name=line[first:stop]   
             if name not in dic_id:
                 dic_id[name]=number_id
                 number_id=number_id+1
-                space=line[0:15]
-                date_time=space.replace(',','')
+                # space=line[0:17]
+                space = line.split()
+                date_time = space[0] + " " + space[1]
+                date_time=date_time.replace(',','')
                 dic['datetime']= date_time
                 dic['id']=number_id-1
                 text = line.split(':')
@@ -33,6 +40,20 @@ def what_App():
                 date=date_time
                 index=number_id-1
                 list_dic.append(dic.copy()) 
+            else:
+                dic['id']=dic_id[name]
+         
+                # space=line[0:17]
+                space = line.split()
+                date_time = space[0] + " " + space[1]
+                date_time=date_time.replace(',','')
+                dic['datetime']= date_time
+                text = line.split(':')
+                dic['text'] = text[2]  
+                date=date_time
+                list_dic.append(dic.copy()) 
+                
+
     load_file = read_file()
     for line_r in load_file:
         line_r=line_r.strip()
@@ -41,26 +62,27 @@ def what_App():
             end=line_r.find('"',start)
             if end>0:
                 find_creator=line_r.split('ידי')
-                data_dic['creation_date']=line_r[0:15]
+                creation_date1=line_r.split()
+                creation_date1=creation_date1[0] + " " + creation_date1[1]
+                creation_date1=creation_date1.replace(',','')
+                
                 c_name=line_r.split('"')
                 c_name=c_name[1].split('"')
                 data_dic['chat_name']=c_name[0]
-                data_dic['creator']=find_creator[1]
+                data_dic['creation_date']=creation_date1
                 data_dic['num_of_participants']=number_id-1
+                data_dic['creator']=find_creator[1]
+               
+
             chat_name1=c_name[0]
-#חלק ה           
+#חלק ה          
     final=dict()
-    final['metadata']=data_dic
     final['messages']=list_dic
+    final['metadata']=data_dic
+
     json_file=chat_name1+".txt"       
     with open(json_file, 'w' , encoding='utf8') as json_file:
         json.dump( final,  json_file , ensure_ascii=False)
-               
-def read_file():
-    file = open("WhatsApp.txt" ,encoding='utf8')
-    return file
-           
-        
-        
-        
+              
+
 what_App()
